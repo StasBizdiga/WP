240dbbc0e2626fc28bf9b2d1f87c9640af90b01b
StasBizdiga <azurium.official@gmail.com>
2017-02-23 15:28:36 +0200

    Base is ready!

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,57 +1,67 @@
 #include <windows.h>
 
-LRESULT CALLBACK WndProc (HWND, UINT, WPARAM, LPARAM) ;
+/*  Declare Windows procedure  */
+LRESULT CALLBACK WindowsProcedure (HWND, UINT, WPARAM, LPARAM) ;
 
 int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
                     PSTR szCmdLine, int iCmdShow)
 {
-     static TCHAR szAppName[] = TEXT ("Lab#1") ;
-     HWND         hwnd ;
-     MSG          msg ;
-     WNDCLASS     wndclass ;
+/*   Set the name for the application  */
+     static TCHAR szAppName[] = TEXT ("Lab_1") ;
 
-     wndclass.style         = CS_HREDRAW | CS_VREDRAW ;
-     wndclass.lpfnWndProc   = WndProc ;
-     wndclass.cbClsExtra    = 0 ;
-     wndclass.cbWndExtra    = 0 ;
-     wndclass.hInstance     = hInstance ;
-     wndclass.hIcon         = LoadIcon (NULL, IDI_APPLICATION) ;
-     wndclass.hCursor       = LoadCursor (NULL, IDC_ARROW) ;
-     wndclass.hbrBackground = (HBRUSH) GetStockObject (WHITE_BRUSH) ;
-     wndclass.lpszMenuName  = NULL ;
-     wndclass.lpszClassName = szAppName ;
+     HWND         hwnd ;      /* Handle for window */
+     message      message ;   /* Store messages here */
+     WNDCLASS     winclass ;  /* WindowClass data structure*/
 
-     if (!RegisterClass (&wndclass))
+     winclass.style         = CS_HREDRAW | CS_VREDRAW ;
+     winclass.lpfnWindowsProcedure   = WindowsProcedure ;
+     winclass.cbClsExtra    = 0 ;
+     winclass.cbWndExtra    = 0 ;
+     winclass.hInstance     = hInstance ;
+     winclass.hIcon         = LoadIcon (NULL, IDI_APPLICATION) ;
+     winclass.hCursor       = LoadCursor (NULL, IDC_ARROW) ;
+     winclass.hbrBackground = (HBRUSH) GetStockObject (WHITE_BRUSH) ;
+     winclass.lpszMenuName  = NULL ;
+     winclass.lpszClassName = szAppName ;
+
+
+/*   If Registering window class fails, close app */
+     if (!RegisterClass (&winclass))
      {
           MessageBox (NULL, TEXT ("This program requires Windows NT!"),
                       szAppName, MB_ICONERROR) ;
           return 0 ;
      }
 
-     hwnd = CreateWindow (szAppName,                  // window class name
-                          TEXT ("Lab#1"),             // window caption
-                          WS_OVERLAPPEDWINDOW,        // window style
-                          CW_USEDEFAULT,              // initial x position
-                          CW_USEDEFAULT,              // initial y position
-                          CW_USEDEFAULT,              // initial x size
-                          CW_USEDEFAULT,              // initial y size
-                          NULL,                       // parent window handle
-                          NULL,                       // window menu handle
-                          hInstance,                  // program instance handle
-                          NULL) ;                     // creation parameters
+/*   Once class is registered, create the window*/
+     hwnd = CreateWindow (szAppName,             // window class name
+                          TEXT ("Lab#1"),        // window caption
+                          WS_OVERLAPPEDWINDOW,   // window style
+                          CW_USEDEFAULT,         // initial x position
+                          CW_USEDEFAULT,         // initial y position
+                          CW_USEDEFAULT,         // initial x size
+                          CW_USEDEFAULT,         // initial y size
+                          NULL,                  // parent window handle
+                          NULL,                  // window menu handle
+                          hInstance,             // program instance handle
+                          NULL) ;                // creation parameters
 
+/*   Make the window visible on the screen */
      ShowWindow (hwnd, iCmdShow) ;
      UpdateWindow (hwnd) ;
 
-     while (GetMessage (&msg, NULL, 0, 0))
+/*   Run the loop until no message is acquired */
+     while (GetMessage (&message, NULL, 0, 0))
      {
-          TranslateMessage (&msg) ;
-          DispatchMessage (&msg) ;
+          TranslateMessage (&message) ;  // Translate virtual-key to char message
+          DispatchMessage (&message) ;   // Send message to windows procedure
      }
-     return msg.wParam ;
+     return message.wParam ;
 }
 
-LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+
+/*  The following function is called by DispatchMessage()  */
+LRESULT CALLBACK WindowsProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
      HDC         hdc ;
      PAINTSTRUCT ps ;
@@ -79,98 +89,3 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
      }
      return DefWindowProc (hwnd, message, wParam, lParam) ;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const char g_szClassName[] = "myWindowClass";
-
-// Step 4: the Window Procedure
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-    switch(msg)
-    {
-        case WM_CLOSE:
-            DestroyWindow(hwnd);
-        break;
-        case WM_DESTROY:
-            PostQuitMessage(0);
-        break;
-        default:
-            return DefWindowProc(hwnd, msg, wParam, lParam);
-    }
-    return 0;
-}
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine, int nCmdShow)
-{
-    WNDCLASSEX wc;
-    HWND hwnd;
-    MSG Msg;
-
-    //Step 1: Registering the Window Class
-    wc.cbSize        = sizeof(WNDCLASSEX);
-    wc.style         = 0;
-    wc.lpfnWndProc   = WndProc;
-    wc.cbClsExtra    = 0;
-    wc.cbWndExtra    = 0;
-    wc.hInstance     = hInstance;
-    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
-    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
-    wc.lpszMenuName  = NULL;
-    wc.lpszClassName = g_szClassName;
-    wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
-
-    if(!RegisterClassEx(&wc))
-    {
-        MessageBox(NULL, "Window Registration Failed!", "Error!",
-            MB_ICONEXCLAMATION | MB_OK);
-        return 0;
-    }
-
-    // Step 2: Creating the Window
-    hwnd = CreateWindowEx(
-        WS_EX_CLIENTEDGE,
-        g_szClassName,
-        "Window of wisdom and zen",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 600, 300,
-        NULL, NULL, hInstance, NULL);
-
-    if(hwnd == NULL)
-    {
-        MessageBox(NULL, "Window Creation Failed!", "Error!",
-            MB_ICONEXCLAMATION | MB_OK);
-        return 0;
-    }
-
-    ShowWindow(hwnd, nCmdShow);
-    UpdateWindow(hwnd);
-
-    // Step 3: The Message Loop
-    while(GetMessage(&Msg, NULL, 0, 0) > 0)
-    {
-        TranslateMessage(&Msg);
-        DispatchMessage(&Msg);
-    }
-    return Msg.wParam;
-}
-*/
