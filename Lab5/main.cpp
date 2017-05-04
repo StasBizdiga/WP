2a942caac64471402ce1562dc596988c86c18081
StasBizdiga <azurium.official@gmail.com>
2017-05-04 17:00:19 +0300

    initial commit

---
+++ Lab5/main.cpp
@@ -0,0 +1,80 @@
+#define _WIN32_WINNT 0x0500
+#include <windows.h>
+#include <commctrl.h>
+#include <cmath>
+#define ID_TIMER    1
+
+LRESULT CALLBACK WndProc   (HWND, UINT, WPARAM, LPARAM) ;
+VOID    CALLBACK TimerProc (HWND, UINT, UINT,   DWORD ) ;
+HINSTANCE hInst;
+
+int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                    PSTR szCmdLine, int iCmdShow)
+{
+     static char szAppName[] = "Lab-5" ;
+     HWND        hwnd ;
+     MSG         msg ;
+     WNDCLASS    wndclass ;
+
+     wndclass.style         = CS_HREDRAW | CS_VREDRAW ;
+     wndclass.lpfnWndProc   = WndProc ;
+     wndclass.cbClsExtra    = 0 ;
+     wndclass.cbWndExtra    = 0 ;
+     wndclass.hInstance     = hInstance ;
+     wndclass.hIcon         = LoadIcon (NULL, IDI_APPLICATION) ;
+     wndclass.hCursor       = LoadCursor (NULL, IDC_ARROW) ;
+     wndclass.hbrBackground = (HBRUSH) GetStockObject (WHITE_BRUSH) ;
+     wndclass.lpszMenuName  = NULL ;
+     wndclass.lpszClassName = szAppName ;
+
+     if (!RegisterClass (&wndclass))
+     {
+          MessageBox (NULL, TEXT ("Program requires Windows NT!"),
+                      szAppName, MB_ICONERROR) ;
+          return 0 ;
+     }
+
+     hwnd = CreateWindow (szAppName, "Lab-5",
+                          WS_OVERLAPPEDWINDOW,
+                          CW_USEDEFAULT, CW_USEDEFAULT,
+                          CW_USEDEFAULT, CW_USEDEFAULT,
+                          NULL, NULL, hInstance, NULL) ;
+
+     ShowWindow (hwnd, iCmdShow) ;
+     UpdateWindow (hwnd) ;
+
+     while (GetMessage (&msg, NULL, 0, 0))
+     {
+          TranslateMessage (&msg) ;
+          DispatchMessage (&msg) ;
+     }
+     return msg.wParam ;
+}
+
+
+LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+     {
+
+     case WM_CREATE:
+          return 0 ;
+
+
+     case WM_PAINT:
+          return 0;
+
+
+     case WM_DESTROY:
+          KillTimer (hwnd, ID_TIMER) ;
+          PostQuitMessage (0) ;
+          return 0 ;
+     }
+     return DefWindowProc (hwnd, message, wParam, lParam) ;
+}
+
+VOID CALLBACK TimerProc (HWND hwnd, UINT message, UINT iTimerID, DWORD dwTime)
+{
+
+}
+
